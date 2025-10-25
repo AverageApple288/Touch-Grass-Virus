89e8b72485a2dbe878b7fed0c68caa8750c5906b
AverageApple288 <fabianbutchart@gmail.com>
2025-10-25 12:43:49 +0100

    Fixed change wallpaper

--- change_wallpaper.py
+++ change_wallpaper.py
@@ -4,4 +4,4 @@ def change_wallpaper(image_path="assets/grass.png"):
     command = f"gsettings set org.gnome.desktop.background picture-uri file://{image_path}"
     os.system(command)
 
-change_wallpaper(image_path="assets/grass.png")
+change_wallpaper(image_path=os.path.abspath("assets/grass.png"))
