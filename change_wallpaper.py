9611cb88d50d92fb9b4050d033dcd0c6e75de21a
AverageApple288 <118760633+AverageApple288@users.noreply.github.com>
2025-10-25 12:46:37 +0100

    Added hyprland support to changing wallpaper

--- change_wallpaper.py
+++ change_wallpaper.py
@@ -1,7 +1,9 @@
 import os
 
 def change_wallpaper(image_path="assets/grass.png"):
-    command = f"gsettings set org.gnome.desktop.background picture-uri file://{image_path}"
-    os.system(command)
+    command_gnome = f"gsettings set org.gnome.desktop.background picture-uri file://{image_path}"
+    os.system(command_gnome)
+    command_hyprland = f"swww img {image_path}"
+    os.system(command_hyprland)
 
 change_wallpaper(image_path=os.path.abspath("assets/grass.png"))
