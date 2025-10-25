ff511d98964aa3553cb8d5b1471d2509fd883d4c
macmillanc <csm31@st-andrews.ac.uk>
2025-10-25 10:33:01 +0100

    mode encrypt.py even even better :)

--- encrypt.py
+++ encrypt.py
@@ -1,3 +1,4 @@
+
 from cryptography.fernet import Fernet
 import os
 # Generate a key
@@ -48,10 +49,18 @@ def decrypt(file):
         f.write(decrypted)
 
 
+def encryptDir(directory):
+    for root, _, files in os.walk(directory):
+        for filename in files:  # loop through files in the current directory
+            path=os.path.join(root, filename)
+            encrypt(path)
 
-directory="../pycharm-2025.2.4"
-for root, _, files in os.walk(directory):
-    for filename in files:  # loop through files in the current directory
-        path=os.path.join(root, filename)
-        encrypt(path)
-        decrypt(path)
+def decryptDir(directory):
+        for root, _, files in os.walk(directory):
+            for filename in files:  # loop through files in the current directory
+                path = os.path.join(root, filename)
+                decrypt(path)
+
+directory="test"
+encryptDir(directory)
+decryptDir(directory)
