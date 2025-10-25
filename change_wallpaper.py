a039573160e88b26f3098dd62ef0910e9a79040e
AverageApple288 <118760633+AverageApple288@users.noreply.github.com>
2025-10-25 12:40:29 +0100

    Added change wallpaper

---
+++ change_wallpaper.py
@@ -0,0 +1,7 @@
+import os
+
+def change_wallpaper(image_path="assets/grass.png"):
+    command = f"gsettings set org.gnome.desktop.background picture-uri file://{image_path}"
+    os.system(command)
+
+change_wallpaper(image_path="assets/grass.png")
